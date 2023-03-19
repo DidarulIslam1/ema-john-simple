dd5b5f0f6fb4b62f9011e2ba6a86d1e17957079a
DidarulIslam1 <didartec85@gmail.com>
2023-03-19 13:47:04 +0600

    faceDB js file name correction(that was (faked))

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb, getStoredCart } from '../../utilities/faked';
+import { addToDb, getStoredCart } from '../../utilities/fakeDb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
